9394d9630d089dbce11fc6ddeb58e04f7862e667
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-21 05:52:15 +0800

    Refactor and refine login implementation
    Former-commit-id: 9e8c45f349181055fb60f9380a3e250371df4d64

--- src/main.py
+++ src/main.py
@@ -1,75 +1,62 @@
 import logging
-import random
-import string
-import tempfile
-from pathlib import Path
+import sys
 from sys import exit
 
-from PIL import Image
-from numpy import array
-from selenium import common, webdriver
-
+from selenium import webdriver
+import selenium.common.exceptions as selenium_exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from config import Config as WDConfig
 import webdriver as wd_manager
 from error import InvalidCredentialsError, TooManyAttemptsError
-from solver.captcha import get_captcha_text
-
-
-def get_random_png(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length)) + ".png"
-
+from solver.captcha import get_captcha_from_browser
+from getpass import getpass
 
 def configure_logging():
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 
-def get_captcha(browser_instance):
-    temp_dir = tempfile.gettempdir()
-    temp_file = str(Path(temp_dir, get_random_png(15)))
-    if browser_instance.save_screenshot(temp_file):
-        image = browser_instance.find_element_by_id("imgCaptcha")
-        left = image.location["x"]
-        top = image.location["y"]
-        right = left + image.size["width"]
-        bottom = top + image.size["height"]
-        captcha = Image \
-            .open(temp_file) \
-            .convert('L') \
-            .crop((left, top, right, bottom))
-        return get_captcha_text(array(captcha))
-    else:
-        raise IOError("Failed to save a screenshot of the login page.")
-
-
-configure_logging()
-webdriver_file = wd_manager.get_driver_package()
-opts = webdriver.ChromeOptions()
-try:
-    browser = webdriver.Chrome(webdriver_file, options=opts)
-except common.exceptions.WebDriverException:
-    logging.critical("You must install Chrome before running this!")
-    exit(1)
-browser.get("https://webap.nptu.edu.tw")
-browser.set_window_size(1280, 720)
-browser.find_element_by_id("LoginDefault_ibtLoginStd").click()
-
-for attempt in range(1, 5):
+def main():
+    username = WDConfig.username
+    password = WDConfig.password
+    if username is None:
+        username = input("Enter your username:")
+    if password is None:
+        password = getpass("Enter your password:")
+    configure_logging()
+    webdriver_file = wd_manager.get_driver_package()
+    opts = webdriver.ChromeOptions()
     try:
-        logging.info(f"Attempting to login... [Attempt: {attempt}/5]")
-        browser.find_element_by_id("LoginStd_txtCheckCode").send_keys(get_captcha(browser))
-        browser.find_element_by_id("LoginStd_txtAccount").send_keys("Account")
-        browser.find_element_by_id("LoginStd_txtPassWord").send_keys("Password")
-        browser.find_element_by_id("LoginStd_ibtLogin").click()
-        alert = browser.switch_to.alert
-        if "驗證碼" in alert.text:
-            alert.accept()
-        if "帳號或密碼錯誤" in alert.text:
-            raise InvalidCredentialsError("Username or password incorrect.")
-        if "錯誤超過三次" in alert.text:
-            raise TooManyAttemptsError('Too many login attempts detected. Try again in 15 minutes.')
-    except common.exceptions.UnexpectedAlertPresentException as alert_exception:
-        logging.debug(alert_exception.alert_text)
-        continue
-    except (InvalidCredentialsError, TooManyAttemptsError) as exception:
-        logging.critical(exception.message)
-        break
+        browser = webdriver.Chrome(webdriver_file, options=opts)
+    except selenium_exceptions.WebDriverException:
+        logging.critical("You must install Chrome before running this!")
+        exit(1)
+    browser.get("https://webap.nptu.edu.tw")
+    browser.set_window_size(1280, 720)
+    browser.find_element_by_id("LoginDefault_ibtLoginStd").click()
+
+    for attempt in range(1, WDConfig.attempts):
+        try:
+            logging.info(f"Attempting to login... [Attempt: {attempt}/{WDConfig.attempts}]")
+            browser.find_element_by_id("LoginStd_txtCheckCode").send_keys(get_captcha_from_browser(browser))
+            browser.find_element_by_id("LoginStd_txtAccount").send_keys(username)
+            browser.find_element_by_id("LoginStd_txtPassWord").send_keys(password)
+            browser.find_element_by_id("LoginStd_ibtLogin").click()
+            WebDriverWait(browser, 3).until(expected_conditions.alert_is_present(), "Expected alert from the login page.")
+            alert = browser.switch_to.alert
+            if "驗證碼" in alert.text:
+                alert.accept()
+                continue
+            if "帳號或密碼錯誤" in alert.text:
+                raise InvalidCredentialsError("Username or password incorrect.")
+            if "錯誤超過三次" in alert.text:
+                raise TooManyAttemptsError('Too many login attempts detected. Try again in 15 minutes.')
+        except (InvalidCredentialsError, TooManyAttemptsError) as exception:
+            logging.critical(exception.message)
+            sys.exit(1)
+        # No alerts, assume logged-in
+        except selenium_exceptions.TimeoutException:
+            break
+
+if __name__ == '__main__':
+    main()
--- src/solver/captcha.py
+++ src/solver/captcha.py
@@ -1,6 +1,11 @@
+import random
+import string
+import tempfile
+from pathlib import Path
+
+from PIL import Image
 from numpy import full, array
 from pytesseract import image_to_string
-from PIL import Image
 
 
 def image_processing(pixel):
@@ -38,3 +43,26 @@ def image_processing(pixel):
 def get_captcha_text(pixel):
     pixel = image_processing(pixel)
     return image_to_string(pixel, config="--dpi 120 --oem 3 -c tessedit_char_whitelist=0123456789")
+
+
+def get_captcha_from_browser(browser_instance):
+    temp_dir = tempfile.gettempdir()
+    temp_file = str(Path(temp_dir, get_random_png(15)))
+    if browser_instance.save_screenshot(temp_file):
+        image = browser_instance.find_element_by_id("imgCaptcha")
+        left = image.location["x"]
+        top = image.location["y"]
+        right = left + image.size["width"]
+        bottom = top + image.size["height"]
+        captcha = Image \
+            .open(temp_file) \
+            .convert('L') \
+            .crop((left, top, right, bottom))
+        return get_captcha_text(array(captcha))
+    else:
+        raise IOError("Failed to save a screenshot of the login page.")
+
+
+def get_random_png(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length)) + ".png"
