7588cccc055840af4c163cad9cd72943d9ba1edf
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-12-20 16:31:16 +0800

    Add initial captcha unit test
    Signed-off-by: Still Hsu <5843208+Still34@users.noreply.github.com>
    
    Former-commit-id: fb4decf301f77f63013fca21f830fde159f73c0e

---
+++ tests/test_tesseract.py
@@ -0,0 +1,28 @@
+import unittest
+from glob import glob
+from os import path
+from pathlib import Path
+
+from numpy import array
+from PIL import Image
+
+from captcha_solver import get_captcha_result
+
+
+class TestCaptcha(unittest.TestCase):
+    def test_captcha(self):
+        cwd = Path(__file__).parent
+        sample_path = Path(f'{cwd}/samples').resolve()
+        print(sample_path)
+        samples = glob(path.join(sample_path, '*.jpg'))
+        print(samples)
+        for sample in samples:
+            image = array(Image.open(sample).convert('L'))
+            result = get_captcha_result(image)
+            expected_result = Path(sample).stem
+            print(f'Expected {expected_result}, got {result}')
+            self.assertEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
